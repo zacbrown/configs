a0341564da41cc74b9d409e3319ffd486f4c4b17
Zac Brown <zac@nworbcaz.com>
2012-02-13 09:52:12 -0800

    add themes

---
+++ .textadept/themes/dark/buffer.lua
@@ -0,0 +1,16 @@
+-- Copyright 2007-2011 Mitchell mitchell<att>caladbolg.net. See LICENSE.
+-- Dark editor theme for Textadept.
+
+local buffer = buffer
+
+-- Folding.
+buffer.property['fold'] = '1'
+buffer.property['fold.by.indentation'] = '1'
+buffer.property['fold.line.comments'] = '0'
+
+-- Tabs and Indentation.
+buffer.tab_width = 4
+buffer.use_tabs = false
+buffer.indent = 4
+buffer.tab_indents = true
+buffer.back_space_un_indents = true
